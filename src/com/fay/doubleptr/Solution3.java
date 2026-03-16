66f658428c61ddc561c918550b98f3c41688b64d
melody <1010483951@qq.com>
2026-03-16 19:06:25 +0800

    add

--- src/com/fay/doubleptr/Solution3.java
+++ src/com/fay/doubleptr/Solution3.java
@@ -1,7 +1,11 @@
 package com.fay.doubleptr;
 
 public class Solution3 {
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+    }
     public int trap(int[] height) {
 
+        return 0;
     }
 }
