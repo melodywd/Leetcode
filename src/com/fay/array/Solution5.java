fbcb2de8dd39868426f598365920ee082b91866f
melody <1010483951@qq.com>
2026-03-23 23:41:47 +0800

    add

--- src/com/fay/array/Solution5.java
+++ src/com/fay/array/Solution5.java
@@ -5,7 +5,21 @@ public class Solution5 {
 
     }
     public static int firstMissingPositive(int[] nums) {
-
-        return 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            // 只需看【1，n】的正数，正确的位置是数值比索引大1
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                // 交换元素，使得每个元素都放在它应该在的位置上
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n+1;
     }
 }
