368cd780875b82563938a7f03061f6caec111507
melody <1010483951@qq.com>
2026-03-23 10:51:29 +0800

    add

--- src/com/fay/array/Solution1.java
+++ src/com/fay/array/Solution1.java
@@ -2,10 +2,18 @@ package com.fay.array;
 
 public class Solution1 {
     public static void main(String[] args) {
-
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
     }
     public static int maxSubArray(int[] nums) {
-
-        return 0;
+        // 记录连续子数组最大和
+        int res = nums[0];
+        // 动态规划变量 dp，记录当前元素结尾的连续子数组最大和
+        int dp = 0;
+        for (int num : nums) {
+            dp = Math.max(dp + num, num);
+            res = Math.max(res,dp);
+        }
+        return res;
     }
 }
