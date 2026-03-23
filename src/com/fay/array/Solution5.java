116f83c7fdd0e9f104874a2fd8aadba88e6f36e6
melody <1010483951@qq.com>
2026-03-23 18:28:24 +0800

    add

---
+++ src/com/fay/array/Solution5.java
@@ -0,0 +1,11 @@
+package com.fay.array;
+
+public class Solution5 {
+    public static void main(String[] args) {
+
+    }
+    public static int firstMissingPositive(int[] nums) {
+
+        return 0;
+    }
+}
