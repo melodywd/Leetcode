c7f39fd6d5c4114ba03e1b12deeb97aa6986d90a
melody <1010483951@qq.com>
2026-04-04 20:15:54 +0800

    add

--- src/com/fay/linkedList/Solution12.java
+++ src/com/fay/linkedList/Solution12.java
@@ -2,6 +2,10 @@ package com.fay.linkedList;
 
 public class Solution12 {
     public ListNode sortList(ListNode head) {
-
+        // 分治
+        // 找中间节点
+        // 归并排序
+        // 合并链表
+        return new ListNode();
     }
 }
