7fe86f9de8251e30281f21b6f8f666d5eb402742
melody <1010483951@qq.com>
2026-04-04 20:43:49 +0800

    add

--- src/com/fay/linkedList/Solution12.java
+++ src/com/fay/linkedList/Solution12.java
@@ -2,10 +2,47 @@ package com.fay.linkedList;
 
 public class Solution12 {
     public ListNode sortList(ListNode head) {
-        // 分治
+        if (head == null || head.next == null) {
+            return head;
+        }
         // 找中间节点
-        // 归并排序
+        ListNode midNode = findMin(head);
+        // 分治
+        head = sortList(head);
+        midNode = sortList(midNode);
         // 合并链表
-        return new ListNode();
+        return merge(head,midNode);
+    }
+    // 找中间节点
+    public static ListNode findMin(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode temp = new ListNode(0);
+        while (fast != null && fast.next != null) {
+            // 记录中间节点的前一个节点
+            temp = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // 将节点从中间断开
+        temp.next = null;
+        return slow;
+    }
+    // 合并链表————双指针
+    public static ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            }else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
     }
 }
