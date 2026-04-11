1ccbec014c107b63dcc4f9900da037535a6e1899
melody <1010483951@qq.com>
2026-04-11 11:25:43 +0800

    add

--- src/com/fay/tree/Solution1.java
+++ src/com/fay/tree/Solution1.java
@@ -9,13 +9,13 @@ public class Solution1 {
         dfs(res, root);
         return res;
     }
-    // 遍历
+    // 中序遍历
     public void dfs(List<Integer> res, TreeNode root) {
         // 递归边界
         if (root == null) return ;
         // 遍历左子树
         dfs(res, root.left);
-        // 添加根节点
+        // 添加根节点（添加根节点的顺序不同实现不同遍历方式）
         res.add(root.val);
         // 遍历右子树
         dfs(res, root.right);
