4aa243e5696d8fcf07cc36c5afd28bde91b8ecb9
melody <1010483951@qq.com>
2026-04-11 18:15:15 +0800

    add

--- src/com/fay/tree/Solution3.java
+++ src/com/fay/tree/Solution3.java
@@ -3,5 +3,12 @@ package com.fay.tree;
 public class Solution3 {
     public TreeNode invertTree(TreeNode root) {
 
+        if (root == null) return null;
+
+        // 翻转左子树和右子树
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
     }
 }
