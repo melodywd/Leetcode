407157eec59f3edf4285de91bb1fc1f6a0517c36
melody <1010483951@qq.com>
2026-04-12 15:24:00 +0800

    add

--- src/com/fay/tree/Solution4.java
+++ src/com/fay/tree/Solution4.java
@@ -7,9 +7,11 @@ public class Solution4 {
     }
     // 判断左右子节点是否为镜像
     public boolean isSame(TreeNode left, TreeNode right) {
+        // 都为空是镜像
         if (left == null && right == null) return true;
-        if (left == null || right == null || left.val != right.val) return false;
-        // 左子节点的左子树和右节点的右子树、左节点的
-        return isSame(left.left, right.right) && isSame(left.right, right.left);
+        // 只有一个为空不是镜像
+        if (left == null || right == null) return false;
+        // 镜像满足条件：节点相等、左左和右右、左右和右左都为镜像
+        return left.val == right.val && isSame(left.left, right.right) && isSame(left.right, right.left);
     }
 }
