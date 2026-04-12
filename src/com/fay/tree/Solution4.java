6b17a761c0e5e2aad7a055a8e7d0917b24260a28
melody <1010483951@qq.com>
2026-04-12 15:09:14 +0800

    add

--- src/com/fay/tree/Solution4.java
+++ src/com/fay/tree/Solution4.java
@@ -3,6 +3,13 @@ package com.fay.tree;
 public class Solution4 {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
-
+        return isSame(root.left, root.right);
+    }
+    // 判断左右子节点是否为镜像
+    public boolean isSame(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null || left.val != right.val) return false;
+        // 左子节点的左子树和右节点的右子树、左节点的
+        return isSame(left.left, right.right) && isSame(left.right, right.left);
     }
 }
