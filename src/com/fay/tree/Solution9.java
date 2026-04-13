79b79fa15dbc097552289234f8f6e24e6623b3dc
melody <1010483951@qq.com>
2026-04-13 21:07:19 +0800

    add

--- src/com/fay/tree/Solution9.java
+++ src/com/fay/tree/Solution9.java
@@ -11,12 +11,13 @@ public class Solution9 {
     }
     // 中序遍历二叉搜索树，第K个节点为第K小的元素
     public void dfs(TreeNode root) {
-        if (root == null || k <= 0) return;
+        if (root == null) return;
         // 左子树
         dfs(root.left);
         k--;
         if (k == 0) {
             res = root.val;
+            return;
         }
         // 右子树
         dfs(root.right);
