477d90ba6492d77b8b425786bddb76a1de7fbf51
melody <1010483951@qq.com>
2026-04-13 11:35:35 +0800

    add

--- src/com/fay/tree/Solution8.java
+++ src/com/fay/tree/Solution8.java
@@ -2,9 +2,11 @@ package com.fay.tree;
 
 public class Solution8 {
     public boolean isValidBST(TreeNode root) {
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    public boolean dfs(TreeNode root,long min, long max) {
         if (root == null) return true;
-        if (root.left == null && root.right == null) return true;
-        if (root.left == null || root.right == null) return false;
-        return  root.val > root.left.val && root.val < root.right.val && isValidBST(root.left) && isValidBST(root.right);
+        if (root.val <= min || root.val >= max) return false;
+        return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
     }
 }
