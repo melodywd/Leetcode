838cd2a34559ce068f3f20b5ca874370b0361f16
melody <1010483951@qq.com>
2026-07-21 10:50:05 +0800

    add

--- src/com/fay/linkedList/LRUCacheACM.java
+++ src/com/fay/linkedList/LRUCacheACM.java
@@ -14,8 +14,6 @@ public class LRUCacheACM {
         Node (int k, int v) {
             key = k;
             value = v;
-            prev = null;
-            next = null;
         }
     }
     // 容量、虚拟头节点、哈希表（存节点）
@@ -30,26 +28,22 @@ public class LRUCacheACM {
         dummy.prev = dummy;
     }
     // 获取节点
-    public int get(int key) {
+    private int get(int key){
         Node node = getNode(key);
         return node == null ? -1 : node.value;
     }
     // 添加节点
-    public void put(int key, int value) {
+    private void put(int key, int value) {
         // 节点存在
         if (NodeMap.containsKey(key)) {
-            // 将节点移到头节点
             Node node = getNode(key);
-            // 更新 value
             node.value = value;
         }else {
-            // 不存在则添加一个新节点，并放在头节点位置
             Node node = new Node(key, value);
-            // 加入哈希表
-            NodeMap.put(key, node);
+            NodeMap.put(key,node);
             addToHead(node);
         }
-        // 超过容量，删除最后操作的节点即尾节点
+        // 超过容量移除尾节点
         if (NodeMap.size() > capacity) {
             Node tail = dummy.prev;
             NodeMap.remove(tail.key);
@@ -58,16 +52,17 @@ public class LRUCacheACM {
     }
     // 获取节点，并将节点移到头节点位置
     private Node getNode(int key) {
+        // 哈希表不存在节点返回 null
         if (!NodeMap.containsKey(key)) {
             return null;
         }
         Node node = NodeMap.get(key);
-        // 移除节点
+        // 移除节点并添加到头节点
         remove(node);
-        // 将节点放在头节点
         addToHead(node);
         return node;
     }
+
     // 移除节点
     private void remove(Node node) {
         node.prev.next = node.next;
@@ -75,10 +70,11 @@ public class LRUCacheACM {
     }
     // 节点移到头节点位置
     private void addToHead(Node node) {
-        node.prev = dummy;
-        node.next = dummy.next;
-        node.prev.next = node;
-        node.next.prev = node;
+       node.next = dummy.next;
+       node.prev = dummy;
+       node.next.prev = node;
+       node.prev.next = node;
+
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -87,14 +83,15 @@ public class LRUCacheACM {
         LRUCacheACM LRU = new LRUCacheACM(capacity);
 
         for (int i = 0; i < n; i++) {
-            if ("put".equals(scanner.next())) {
+            String operation = scanner.next();
+            if ("put".equals(operation)) {
                 int key = scanner.nextInt();
                 int value = scanner.nextInt();
                 LRU.put(key, value);
-            }else if ("get".equals(scanner.next())) {
+            }else if ("get".equals(operation)) {
                 int key = scanner.nextInt();
                 int value = LRU.get(key);
-                System.out.println(value + " " + LRU.get(key));
+                System.out.println(value);
             }
         }
     }
