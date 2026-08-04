73e684d3bde66ca3f92c8a8380b3d4feb65f2e57
melody <1010483951@qq.com>
2026-08-04 10:29:20 +0800

    8.4

--- src/com/fay/binarysearch/Solution6ACM.java
+++ src/com/fay/binarysearch/Solution6ACM.java
@@ -1,41 +1,54 @@
 package com.fay.binarysearch;
 
+import java.util.Scanner;
+
 public class Solution6ACM {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // 确保 nums1 是短的那个数组
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[] nums1 = new int[m];
+        int[] nums2 = new int[n];
+        for (int i = 0; i < m; i++) {
+            nums1[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            nums2[i] = sc.nextInt();
+        }
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         if (nums1.length > nums2.length) {
-            int[] temp = nums1;
-            nums1 = nums2;
-            nums2 = temp;
+            int[] temp = nums2;
+            nums2 = nums1;
+            nums1 = temp;
         }
         int m = nums1.length;
         int n = nums2.length;
-        // 奇数或偶数中位数的位置
-        int half = (m + n + 1)/2;
-        // 二分查找边界，闭区间
-        int left = 0;
-        int right = m;
-        while (left <= right) {
-            // 两个数组切割线的位置
-            int i = (left + right)/2;// 左边 i 个数
-            int j = half - i;// 左边 j 个数
-            // 考虑边界，切割线左边或右边没有数字
-            // 没有数字一边设置为最小值或最大值，使满足判断条件
+        int half = (m+n+1)/2;
+
+        int l = 0;
+        int r = m;
+        while (l <= r) {
+            int i = l + (r - l) / 2;
+            int j = half - i;
+            // i 表示数组分割线位置，左边有i个数
+            // 边界是0和m
             int l1 = (i == 0) ? Integer.MIN_VALUE : nums1[i-1];
-            int r1 = (i == m) ? Integer.MAX_VALUE : nums1[i];
             int l2 = (j == 0) ? Integer.MIN_VALUE : nums2[j-1];
+            int r1 = (i == m) ? Integer.MAX_VALUE : nums1[i];
             int r2 = (j == n) ? Integer.MAX_VALUE : nums2[j];
-            // 满足条件：切割线左边数小于等于右边
-            if (l1 <= r2 && l2 <= r1) {
-                if ( (m+n) % 2 == 1) {
-                    return Math.max(l1,l2);
-                }else {
-                    return (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
-                }
+
+            if (l1 <= r2 && l2 < r1) {
+               if ((m+n)%2== 1) {
+                   return Math.max(l1,l2);
+               }else {
+                   return (Math.max(l1,l2) + Math.min(r1,r2)) / 2.0;
+               }
             }else if (l1 > r2) {
-                right = i - 1;
+                r = i - 1;
             }else {
-                left = i + 1;
+                l = i + 1;
             }
         }
         return 0;
