4d5530af9b1b12c1317d2ae32fe3551e99f327c8
melody <1010483951@qq.com>
2026-08-15 12:40:58 +0800

    8.15

--- src/com/fay/tree/Solution5ACM.java
+++ src/com/fay/tree/Solution5ACM.java
@@ -1,10 +1,51 @@
 package com.fay.tree;
+import java.util.*;
 
 public class Solution5ACM {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        List<Integer> nums = new ArrayList<>();
+        while (sc.hasNextInt()) {
+            nums.add(sc.nextInt());
+        }
+        if (nums.isEmpty() || nums.get(0) == -1) return;
+        TreeNode root = buildTree(nums);
+        System.out.println(diameterOfBinaryTree(root));
     }
-    public int diameterOfBinaryTree(TreeNode root) {
-        return 0;
+    public static int res = 0;
+    public static int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+    public static int dfs(TreeNode root) {
+        if (root == null) return -1;
+        int left = dfs(root.left) + 1;
+        int right = dfs(root.right) + 1;
+        res = Math.max(res, left + right);
+        return Math.max(left, right);
+    }
+    public static TreeNode buildTree(List<Integer> nums) {
+        TreeNode root = new TreeNode(nums.get(0));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int index = 1;
+        while (!queue.isEmpty() && index < nums.size()) {
+            TreeNode cur = queue.poll();
+            if (index < nums.size()) {
+                if (nums.get(index) != -1) {
+                    cur.left = new TreeNode(nums.get(index));
+                    queue.offer(cur.left);
+                }
+                index++;
+            }
+            if (index < nums.size()) {
+                if (nums.get(index) != -1) {
+                    cur.right = new TreeNode(nums.get(index));
+                    queue.offer(cur.right);
+                }
+                index++;
+            }
+        }
+        return root;
     }
 }
---
+++ src/com/fay/tree/Solution6ACM.java
@@ -0,0 +1,13 @@
+package com.fay.tree;
+import java.util.*;
+
+public class Solution6ACM {
+    public static void main(String[] args) {
+
+    }
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        return res;
+    }
+}
