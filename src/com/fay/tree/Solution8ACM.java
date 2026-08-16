1d760508cc7b739ec52a40250ec86fba75fc16d5
melody <1010483951@qq.com>
2026-08-16 13:11:08 +0800

    8.16

--- src/com/fay/tree/Solution8ACM.java
+++ src/com/fay/tree/Solution8ACM.java
@@ -1,18 +1,47 @@
 package com.fay.tree;
+import java.util.*;
 
 public class Solution8ACM {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> nums = new ArrayList<>();
+        while (scanner.hasNextInt()) {
+            nums.add(scanner.nextInt());
+        }
+        if (nums.isEmpty() || nums.get(0) == -1) return;
+        TreeNode root = buildTree(nums);
+        System.out.println(isValidBST(root));
     }
     public static boolean isValidBST(TreeNode root) {
         return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
     public static boolean dfs(TreeNode root,long min, long max) {
         if (root == null) return true;
-        // 不满足在最值范围内返回 false
         if (root.val <= min || root.val >= max) return false;
-        // 更新最值为根节点值，判断左子树和右子树
-        return dfs(root.left, min, root.val)
-                && dfs(root.right, root.val, max);
+        return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
+    }
+    public static TreeNode buildTree(List<Integer> nums) {
+        TreeNode root = new TreeNode(nums.get(0));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int index = 1;
+        while (!queue.isEmpty() && index < nums.size()) {
+            TreeNode cur = queue.poll();
+            if (index < nums.size()) {
+                if (nums.get(index) != -1) {
+                    cur.left = new TreeNode(nums.get(index));
+                    queue.offer(cur.left);
+                }
+                index++;
+            }
+            if (index < nums.size()) {
+                if (nums.get(index) != -1) {
+                    cur.right = new TreeNode(nums.get(index));
+                    queue.offer(cur.right);
+                }
+                index++;
+            }
+        }
+        return root;
     }
 }
