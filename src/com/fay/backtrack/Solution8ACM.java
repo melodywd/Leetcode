9757b3b1419334c1001238e0531f2ae024d7df64
melody <1010483951@qq.com>
2026-09-03 15:04:56 +0800

    9.3

--- src/com/fay/backtrack/Solution8ACM.java
+++ src/com/fay/backtrack/Solution8ACM.java
@@ -11,15 +11,24 @@ public class Solution8ACM {
         boolean[] col = new boolean[n];
         boolean[] d1 = new boolean[2*n - 1];
         boolean[] d2 = new boolean[2*n - 1] ;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = '.';
+            }
+        }
         dfs(0, board, col, d1, d2, ans);
         return ans;
     }
 
     public static void dfs(int r, char[][] board, boolean[] col, boolean[] d1, boolean[] d2, List<List<String>> ans) {
         if (r == col.length) {
+            List<String> path = new ArrayList<>();
             for (char[] row : board) {
-                ans.add(new String(row));
+                path.add(new String(row));
             }
+            ans.add(path);
+            return;
         }
+
     }
 }
